0602148949ef1d0d1a2c3d6e1fce7086959e8898
snowflake20241027 <snowflakehencinas@outlook.com>
2024-08-12 19:32:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+fv_df =  st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 
 
